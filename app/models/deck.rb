64e7f7700ab76ef42d9e463e3663866c183f8257
John Turner <groups@norgg.org>
2010-01-30 00:58:51 +0000

    Aces should be As.  Replenish deck from discards.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -6,7 +6,7 @@ class Deck < ActiveRecord::Base
 
   def generate
     self.discard_counter = 1
-    ranks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
     suits = ["♠", "♥", "♦", "♣"]
     ranks.each do |rank|
       suits.each do |suit|
@@ -30,10 +30,27 @@ class Deck < ActiveRecord::Base
     Card.find_last_by_deck_id self.id, :order => :discarded, :conditions => ['discarded >= 0']
   end
 
+  def discards
+    Card.find_all_by_deck_id self.id, :order => :discarded, :conditions => ['discarded >= 0']
+  end
+
   def deal player, num_cards = 1
     dealt_cards = []
     num_cards.times do
       deck_cards = self.cards_in_deck
+      
+      if deck_cards.empty?
+        top = discard_top
+        discards.each do |card|
+          next if card == top
+          card.in_deck = true
+          card.player = nil
+          card.discarded = nil
+          card.save!
+        end
+        deck_cards = self.cards_in_deck
+      end
+
       card = deck_cards[rand deck_cards.size]
       card.in_deck = false
       card.player = player
--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(:version => 20100128151956) do
 
   create_table "decks", :force => true do |t|
     t.integer  "game_id"
-    t.integer  "last_player_id"
     t.integer  "discard_counter"
     t.datetime "created_at"
     t.datetime "updated_at"
